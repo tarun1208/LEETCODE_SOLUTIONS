ecd4eb6766d84d7e950a3b0787d1c4d9aee04db0
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-07-13 12:12:22 +0530

    Create 10-RegularExpressionMatching

---
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.java
@@ -0,0 +1,17 @@
+// Last updated: 7/13/2026, 12:12:22 PM
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if (p.isEmpty()) return s.isEmpty();
+
+        boolean firstMatch = (!s.isEmpty() &&
+                             (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.'));
+
+        if (p.length() >= 2 && p.charAt(1) == '*') {
+            return isMatch(s, p.substring(2)) ||
+                   (firstMatch && isMatch(s.substring(1), p));
+        } else {
+            return firstMatch && isMatch(s.substring(1), p.substring(1));
+        }
+    }
+}
+    
