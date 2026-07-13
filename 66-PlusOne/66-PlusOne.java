ebb3da5c3a9376fe676becab928eeb8c95aefd45
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-07-13 12:11:42 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.java
@@ -0,0 +1,16 @@
+// Last updated: 7/13/2026, 12:11:42 PM
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n =digits.length;
+        int[] res = new int[n+ 1];
+        for (int i = n- 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        res[0] = 1;
+        return res;
+    }
+}
