ab18baf0feb77827d7f87e397333cbc818be113b
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-07-13 12:10:44 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,14 @@
+// Last updated: 7/13/2026, 12:10:44 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        int n =nums.length;
+        int low=0,high = n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid] == target ) return mid;
+            else if(target > nums[mid]) low = mid+1;
+            else high = mid-1;
+        }
+        return -1;
+    }
+}
