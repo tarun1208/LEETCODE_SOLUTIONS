ff91b53da30fa27de8e5bbe6d483b0b150fa0a26
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-07-13 12:10:16 +0530

    Create 1222-RemoveCoveredIntervals

---
+++ 1222-RemoveCoveredIntervals/1222-RemoveCoveredIntervals.java
@@ -0,0 +1,14 @@
+// Last updated: 7/13/2026, 12:10:16 PM
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->a[0]!=b[0]?a[0]-b[0]:b[1]-a[1]);
+        int res=0,r=0;
+        for(int []row:intervals){
+            if(row[1]>r){
+                r=row[1];
+                res++;
+            }
+        }
+        return res;
+    }
+}
