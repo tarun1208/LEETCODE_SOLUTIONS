a6a012f26719af72a799522bc0e730019bc25dfd
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-07-14 15:58:45 +0530

    File updated at 7/14/2026, 3:58:43 PM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,21 +1,24 @@
-// Last updated: 7/14/2026, 3:46:40 PM
+// Last updated: 7/14/2026, 3:58:43 PM
 1class Solution {
 2    public boolean isAnagram(String s, String t) {
-3        if(s.length() != t.length()){
-4            return false;
-5        }
-6
-7        int count[] = new int[26];
-8        
+3
+4        if (s.length() != t.length()) {
+5            return false;
+6        }
+7
+8        HashMap<Character,Integer> map1 = new HashMap<>();
 9        for(int i=0;i<s.length();i++){
-10            count[s.charAt(i)-'a']++;
-11            count[t.charAt(i)-'a']--;
+10            char c = s.charAt(i);
+11            map1.put(c, map1.getOrDefault(c, 0) + 1);
 12        }
-13        for(int val : count){
-14            if(val != 0){
-15                return false;
-16            }
+13        HashMap<Character,Integer> map2 = new HashMap<>();
+14        for(int i=0;i<t.length();i++){
+15            char c = t.charAt(i);
+16            map2.put(c, map2.getOrDefault(c, 0) + 1);
 17        }
-18        return true;
-19    }
-20}
+18        if(map1.equals(map2)){
+19            return true;
+20        }
+21        return false;
+22    }
+23}
