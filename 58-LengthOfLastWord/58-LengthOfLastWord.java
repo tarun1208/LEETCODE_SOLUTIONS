e792f148d1a9168cdc1c59e98c1d04f5b94e2178
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-07-14 21:34:19 +0530

    Create 58-LengthOfLastWord

---
+++ 58-LengthOfLastWord/58-LengthOfLastWord.java
@@ -0,0 +1,15 @@
+// Last updated: 7/14/2026, 9:34:18 PM
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int len = 0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i) != ' '){
+                len++;
+            }
+            else if(len>0){
+                break;
+            }
+        }
+        return len;
+    }
+}
