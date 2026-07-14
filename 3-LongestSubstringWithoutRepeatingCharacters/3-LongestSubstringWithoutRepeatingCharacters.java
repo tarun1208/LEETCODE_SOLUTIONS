84ec38d489eab98d4ddfa483c12185cf4d4c8775
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-07-14 21:39:16 +0530

    File updated at 7/14/2026, 9:39:15 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,18 +1,18 @@
-// Last updated: 7/13/2026, 12:12:41 PM
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int max = 0;
-        String current = "";
-
-        for (int i = 0; i < s.length(); i++) {
-            String ch = String.valueOf(s.charAt(i));
-            if (current.contains(ch)) {
-                int index = current.indexOf(ch);
-                current = current.substring(index + 1);
-            }
-            current += ch;
-            max = Math.max(max, current.length());
-        }
-        return max;
-    }
-}
+// Last updated: 7/14/2026, 9:39:15 PM
+1class Solution {
+2    public int lengthOfLongestSubstring(String s) {
+3        int max = 0;
+4        String current = "";
+5
+6        for (int i = 0; i < s.length(); i++) {
+7            String ch = String.valueOf(s.charAt(i));
+8            if (current.contains(ch)) {
+9                int index = current.indexOf(ch);
+10                current = current.substring(index + 1);
+11            }
+12            current += ch;
+13            max = Math.max(max, current.length());
+14        }
+15        return max;
+16    }
+17}
