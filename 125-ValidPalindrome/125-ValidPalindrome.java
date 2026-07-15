44ae51be3976329f3cd4c9d0a30231df5225bf37
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-07-15 11:23:36 +0530

    File updated at 7/15/2026, 11:23:35 AM

--- 125-ValidPalindrome/125-ValidPalindrome.java
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -1,24 +1,17 @@
-// Last updated: 7/15/2026, 11:16:45 AM
+// Last updated: 7/15/2026, 11:23:35 AM
 1class Solution {
 2    public boolean isPalindrome(String s) {
-3        int start = 0;
-4        int end = s.length()-1;
-5
-6        while(start<end){
-7            if(!Character.isLetterOrDigit(s.charAt(start))){
-8                start++;
-9                continue;
+3        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+4        int l=0;
+5        int n=s.length();
+6        int r=n-1;
+7        while (l < r) {
+8            if (s.charAt(l) != s.charAt(r)) {
+9                return false;
 10            }
-11            if(!Character.isLetterOrDigit(s.charAt(end))){
-12                end--;
-13                continue;
-14            }
-15            if(Character.toLowerCase(s.charAt(start))!=Character.toLowerCase(s.charAt(end))){
-16                return false;
-17            }
-18            start++;
-19            end--;
-20        }
-21        return true;
-22    }
-23}
+11            l++;
+12            r--;
+13        }        
+14        return true;
+15    }
+16}
