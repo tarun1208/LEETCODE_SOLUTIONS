7bf55701239c3da3a66d643bfa022fd02d67e2ea
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-07-15 11:35:01 +0530

    File updated at 7/15/2026, 11:35:00 AM

--- 3658-GCDofOddandEvenSums/3658-GCDofOddandEvenSums.java
+++ 3658-GCDofOddandEvenSums/3658-GCDofOddandEvenSums.java
@@ -1,15 +1,6 @@
-// Last updated: 7/15/2026, 9:44:52 AM
+// Last updated: 7/15/2026, 11:35:00 AM
 1class Solution {
 2    public int gcdOfOddEvenSums(int n) {
-3        int sumOdd = 0;
-4        int sumEven = 0;
-5        for(int i = 1;i<=2*n;i++){
-6            if(i%2!=0){
-7                sumOdd +=i;
-8            }else{
-9                sumEven +=i;
-10            }
-11        }
-12        return sumEven - sumOdd;
-13    }
-14}
+3        return n;
+4    }
+5}
