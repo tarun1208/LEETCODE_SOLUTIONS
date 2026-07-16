17c6e3eb2b82cad6385908dbb74bfa90d3449bf2
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-07-16 14:48:33 +0530

    Create 4242-SumOfGcdOfFormedPairs

---
+++ 4242-SumOfGcdOfFormedPairs/4242-SumOfGcdOfFormedPairs.java
@@ -0,0 +1,33 @@
+// Last updated: 7/16/2026, 2:48:33 PM
+class Solution {
+    public long gcdSum(int[] nums) {
+        int n = nums.length;
+        int [] arr = new int[n];
+        int max = 0;
+        for(int i=0;i<n;i++){
+            max = Math.max(max,nums[i]);
+            arr[i] = gcd(max,nums[i]);
+        }
+
+        Arrays.sort(arr);
+        long sum = 0;
+        int left=0;
+        int right = n-1;
+
+        while(left<right){
+            sum+=gcd(arr[left],arr[right]);
+            left++;
+            right--;
+        }
+        return sum;
+    }
+
+    private int gcd(int a, int b){
+        while(b!=0){
+            int temp = b;
+            b = a%b;
+            a = temp;
+        }
+        return a;
+    }
+}
