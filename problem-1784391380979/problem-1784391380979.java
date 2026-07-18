f8f34d6a7edfdc975daca1354ca52dc9680353c0
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-07-18 21:46:21 +0530

    Create

---
+++ problem-1784391380979/problem-1784391380979.java
@@ -0,0 +1,22 @@
+// Last updated: 7/18/2026, 9:46:20 PM
+1class Solution {
+2    public int minAdjacentSwaps(int[] nums, int a, int b) {
+3        int f = 0;
+4        long count1 = 0;
+5        long count2 = 0;
+6        long swap = 0;
+7        long MOD = 1000000007;
+8
+9        for(int num:nums){
+10            if(num<a){
+11                swap+=(count1+count2);
+12            }else if(num>=a && num<=b){
+13                swap +=count2;
+14                count1++;
+15            }else{
+16                count2++;
+17            }
+18        }
+19        return (int) (swap%MOD);
+20    }
+21}
