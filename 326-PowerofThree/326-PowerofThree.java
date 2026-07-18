913220d166814a99abbd2b7c7439084bc2980f5d
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-07-18 11:30:45 +0530

    File updated at 7/18/2026, 11:30:46 AM

--- 326-PowerofThree/326-PowerofThree.java
+++ 326-PowerofThree/326-PowerofThree.java
@@ -1,4 +1,4 @@
-// Last updated: 7/18/2026, 11:28:39 AM
+// Last updated: 7/18/2026, 11:30:46 AM
 1class Solution {
 2    public boolean isPowerOfThree(int n) {
 3        return n > 0 && 1162261467 % n == 0;
