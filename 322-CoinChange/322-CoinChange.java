4cd68346112a4484660138cdbb0d7d9a4c50af9d
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-07-22 14:33:53 +0530

    File updated at 7/22/2026, 2:33:52 PM

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,31 +1,18 @@
-// Last updated: 7/22/2026, 2:33:39 PM
+// Last updated: 7/22/2026, 2:33:52 PM
 1class Solution {
 2    public int coinChange(int[] coins, int amount) {
-3        int n = coins.length;
-4        int [] dp = new int[amount+1];
-5
-6        if(amount<1) return 0;
-7
-8        return solve(coins,amount,dp);
-9    }
-10    
-11    private int solve(int[] coins, int rem, int[] dp){
-12
-13        if(rem < 0) return -1;
-14        if(rem == 0) return 0;
-15        if(dp[rem] !=0) return dp[rem];
-16
-17        int minCoins = Integer.MAX_VALUE;
-18
-19        for (int coin : coins) {
-20            int res = solve(coins, rem - coin, dp);
-21
-22            if (res >= 0 && res < minCoins) {
-23                minCoins = 1 + res;
-24            }
-25        }
-26
-27        dp[rem] = (minCoins == Integer.MAX_VALUE) ? -1 : minCoins;
-28        return dp[rem];
-29    }
-30}
+3        int[] minCoins = new int[amount + 1];
+4        Arrays.fill(minCoins, amount + 1);
+5        minCoins[0] = 0;
+6
+7        for (int i = 1; i <= amount; i++) {
+8            for (int j = 0; j < coins.length; j++) {
+9                if (i - coins[j] >= 0) {
+10                    minCoins[i] = Math.min(minCoins[i], 1 + minCoins[i - coins[j]]);
+11                }
+12            }
+13        }
+14
+15        return minCoins[amount] != amount + 1 ? minCoins[amount] : -1;        
+16    }
+17}
