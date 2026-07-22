cd1c9d1f8fd3e5bdf373ce4c9a6f188163b40795
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-07-22 10:10:48 +0530

    File updated at 7/22/2026, 10:10:47 AM

--- 509-FibonacciNumber/509-FibonacciNumber.java
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -1,9 +1,14 @@
-// Last updated: 7/22/2026, 10:07:08 AM
+// Last updated: 7/22/2026, 10:10:47 AM
 1class Solution {
 2    public int fib(int n) {
 3        if(n<=1) return n;
-4        else{
-5            return fib(n-1)+fib(n-2);
-6        }
-7    }
-8}
+4        
+5        int[] dp = new int[n+1];
+6        dp[0] = 0;
+7        dp[1] = 1;
+8        for(int i=2;i<=n;i++){
+9            dp[i] = dp[i-1]+dp[i-2];
+10        }
+11        return dp[n];
+12    }
+13}
