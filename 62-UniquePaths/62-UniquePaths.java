03a88c573d7e237de0ecec2e290e6c0a48c575aa
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-07-22 14:20:57 +0530

    File updated at 7/22/2026, 2:20:56 PM

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,24 +1,15 @@
-// Last updated: 7/22/2026, 2:12:42 PM
+// Last updated: 7/22/2026, 2:20:56 PM
 1class Solution {
-2    int dp[][];
-3    public int uniquePaths(int m, int n) {
-4
-5        dp = new int[m][n];
-6        for(int row[]:dp)
-7            Arrays.fill(row,-1);
-8
-9        return solve(0,0,m,n);
-10    }
+2    public int uniquePaths(int m, int n) {
+3        int dp[] = new int[n];
+4        Arrays.fill(dp,1);
+5
+6        for(int i=1;i<m;i++){
+7            for(int j=1; j<n; j++){
+8                dp[j] = dp[j] + dp[j-1];
+9            }
+10        }
 11
-12    private int solve(int i,int j,int m, int n){
-13        if(i>=m || j>=n) return 0;
-14        if(i == m-1 && j == n-1) return 1;
-15        if (dp[i][j] != -1) return dp[i][j];
-16        
-17        int down = solve(i+1,j,m,n);
-18        int right = solve(i,j+1,m,n);
-19
-20        dp[i][j] = down + right;
-21        return dp[i][j];
-22    }
-23}
+12        return dp[n-1];
+13    }
+14}
