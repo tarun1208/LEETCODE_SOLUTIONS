9065092f58fe632c376dc6d9d6e0878ff59ada2a
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-08-01 11:06:47 +0530

    Create 486-PredictTheWinner

---
+++ 486-PredictTheWinner/486-PredictTheWinner.java
@@ -0,0 +1,15 @@
+// Last updated: 8/1/2026, 11:06:46 AM
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        if (n % 2 == 0) return true;
+        
+        int[] dp = nums.clone();
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            }
+        }
+        return dp[n - 1] >= 0;
+    }
+}
