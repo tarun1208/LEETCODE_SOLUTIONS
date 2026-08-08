81b151e4727e0c7e2e6198959b318b5b8aadb003
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-08-08 20:34:23 +0530

    Create 3584-FindTheLexicographicallySmallestValidSequence

---
+++ 3584-FindTheLexicographicallySmallestValidSequence/3584-FindTheLexicographicallySmallestValidSequence.java
@@ -0,0 +1,45 @@
+// Last updated: 8/8/2026, 8:34:22 PM
+class Solution {
+    public int[] validSequence(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int[] dp = new int[n + 1];
+        int j = m - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = dp[i + 1];
+
+            if (j >= 0 && word1.charAt(i) == word2.charAt(j)) {
+                dp[i]++;
+                j--;
+            }
+        }
+
+        int[] ans = new int[m];
+        int i = 0;
+        j = 0;
+
+        while (i < n && j < m) {
+
+            if (word1.charAt(i) == word2.charAt(j)) {
+                ans[j++] = i;
+            }
+
+            else if (dp[i + 1] >= m - j - 1) {
+                ans[j++] = i++;
+                break;
+            }
+
+            i++;
+        }
+
+        while (i < n && j < m) {
+            if (word1.charAt(i) == word2.charAt(j)) {
+                ans[j++] = i;
+            }
+            i++;
+        }
+
+        return j == m ? ans : new int[0];
+    }
+}
