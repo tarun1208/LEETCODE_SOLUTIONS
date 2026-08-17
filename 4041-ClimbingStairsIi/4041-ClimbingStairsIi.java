019418b37e8095429918654e0ad9187588055aed
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-08-17 20:00:20 +0530

    Create 4041-ClimbingStairsIi

---
+++ 4041-ClimbingStairsIi/4041-ClimbingStairsIi.java
@@ -0,0 +1,20 @@
+// Last updated: 8/17/2026, 8:00:19 PM
+class Solution {
+    public int climbStairs(int n, int[] costs) {
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+
+        for(int i=1;i<=n; i++){
+            int min = Integer.MAX_VALUE;
+
+            for(int j=1;j<=3;j++){
+                if(i-j >= 0){
+                    int cost = costs[i-1] + (j*j);
+                    min = Math.min(min,dp[i-j]+cost);
+                }
+            }
+            dp[i] = min;
+        }
+        return dp[n];
+    }
+}
