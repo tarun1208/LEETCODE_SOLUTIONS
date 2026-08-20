3f61ca248d3dc9080ca5120b92b21e71f5335e9b
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-08-20 08:26:46 +0530

    Create 3347-DistributeElementsIntoTwoArraysI

---
+++ 3347-DistributeElementsIntoTwoArraysI/3347-DistributeElementsIntoTwoArraysI.java
@@ -0,0 +1,32 @@
+// Last updated: 8/20/2026, 8:26:46 AM
+class Solution {
+    public int[] resultArray(int[] nums) {
+        int n = nums.length;
+        List<Integer> arr1 = new ArrayList<>();
+        List<Integer> arr2 = new ArrayList<>();
+        arr1.add(nums[0]);
+        arr2.add(nums[1]);
+
+        for(int i=2;i<n;i++){
+            int last1 = arr1.get(arr1.size()-1);
+            int last2 = arr2.get(arr2.size()-1);
+
+            if(last1>last2){
+                arr1.add(nums[i]);
+            }
+            else{
+                arr2.add(nums[i]);
+            }
+        }
+
+        int []result = new int[n];
+        int index = 0;
+        for(int num : arr1){
+            result[index++] = num;
+        }
+        for(int num : arr2){
+            result[index++] = num;
+        }
+        return result;
+    }
+}
