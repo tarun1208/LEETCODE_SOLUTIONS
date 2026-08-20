0338ffacfc479ee9779f0c87f79d794f0a0836aa
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-08-20 16:52:52 +0530

    Create 416-PartitionEqualSubsetSum

---
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
@@ -0,0 +1,12 @@
+// Last updated: 8/20/2026, 4:52:52 PM
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[][] mat1 = {{3, 1, 2}, {4, 8, 5}, {6, 9, 7}};
+        System.out.println(solution.zigzagSequence(mat1)); // Output: 18
+
+        int[][] mat2 = {{1, 2, 4}, {3, 9, 6}, {11, 3, 15}};
+        System.out.println(solution.zigzagSequence(mat2)); // Output: 28
+    }
+}
