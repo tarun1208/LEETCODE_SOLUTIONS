9d0de5218f9aa8467e0661b56e31ad4341fee566
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-08-23 09:53:49 +0530

    Create 2039-SumGame

---
+++ 2039-SumGame/2039-SumGame.java
@@ -0,0 +1,30 @@
+// Last updated: 8/23/2026, 9:53:49 AM
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int leftSum = 0;
+        int rightSum = 0;
+        int leftQ = 0;
+        int rightQ = 0;
+
+        for (int i = 0; i < n/2; i++) {
+            if (num.charAt(i) == '?'){
+                leftQ++;
+            }
+            else{
+                leftSum += num.charAt(i) - '0';
+            }
+        }
+
+        for (int i = n/2; i < n; i++) {
+            if (num.charAt(i) == '?'){
+                rightQ++;
+            }
+            else{
+                rightSum += num.charAt(i) - '0';
+            }
+        }
+
+        return (leftSum - rightSum) * 2 != (rightQ - leftQ) * 9;
+    }
+}
