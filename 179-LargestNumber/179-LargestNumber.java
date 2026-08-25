a3003fa3f82fa95133486b3807d8274815df3fee
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-08-25 10:51:45 +0530

    Create 179-LargestNumber

---
+++ 179-LargestNumber/179-LargestNumber.java
@@ -0,0 +1,12 @@
+// Last updated: 8/25/2026, 10:51:44 AM
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[][] mat1 = {{3, 1, 2}, {4, 8, 5}, {6, 9, 7}};
+        System.out.println(solution.zigzagSequence(mat1)); // Output: 18
+
+        int[][] mat2 = {{1, 2, 4}, {3, 9, 6}, {11, 3, 15}};
+        System.out.println(solution.zigzagSequence(mat2)); // Output: 28
+    }
+}
