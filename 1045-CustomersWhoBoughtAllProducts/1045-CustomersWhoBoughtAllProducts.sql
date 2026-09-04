46e1a556a34eb87d594c8f236b910cb842a9e5e2
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-09-04 21:04:08 +0530

    Create 1045-CustomersWhoBoughtAllProducts

---
+++ 1045-CustomersWhoBoughtAllProducts/1045-CustomersWhoBoughtAllProducts.sql
@@ -0,0 +1,7 @@
+-- Last updated: 9/4/2026, 9:04:07 PM
+# Write your MySQL query statement below
+select customer_id
+from Customer
+group by customer_id
+having count(distinct product_key) = (select count(product_key)
+                                        from Product)
