128665443c0bff34a359311ebab3277c974a59d8
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-09-04 21:02:13 +0530

    Create 1652-DefuseTheBomb

---
+++ 1652-DefuseTheBomb/1652-DefuseTheBomb.java
@@ -0,0 +1,30 @@
+// Last updated: 9/4/2026, 9:02:12 PM
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int ans[] = new int[n];
+        if(k==0){
+            return ans;
+        }
+        else if (k>0){
+            for(int i=0;i<n;i++){
+                int sum = 0;
+                for(int j=1;j<=k;j++){
+                    sum +=code[(i+j)%n];
+                } 
+                ans[i] = sum;
+            }
+            return ans;
+        }
+        else{
+            for(int i=0;i<n;i++){
+                int sum =0;
+                for(int j=1;j<=-k;j++){
+                    sum +=code[(i-j+n)%n];
+                }
+                ans[i] = sum;
+            }
+            return ans;
+        }
+    }
+}
