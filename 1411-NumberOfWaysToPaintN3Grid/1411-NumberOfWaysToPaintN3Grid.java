22886475ea56036a4aa24918429c56e360ddd801
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-09-04 21:02:40 +0530

    Create 1411-NumberOfWaysToPaintN3Grid

---
+++ 1411-NumberOfWaysToPaintN3Grid/1411-NumberOfWaysToPaintN3Grid.java
@@ -0,0 +1,15 @@
+// Last updated: 9/4/2026, 9:02:40 PM
+class Solution {
+    public int numOfWays(int n) {
+        int modulo = 1000000007;
+        long alternative = 6;
+        long different = 6;
+        for(int i=2; i<=n;i++){
+            long new_alt = (3*alternative + 2*different)%modulo;
+            long new_diff = (2*alternative + 2*different)%modulo;
+            alternative = new_alt;
+            different = new_diff;
+        }
+        return(int)((alternative + different)%modulo);
+    }
+}
