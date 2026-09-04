6ba3782f3d948549779416ca4af5795084f59717
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-09-04 21:01:23 +0530

    Create 1978-EmployeesWhoseManagerLeftTheCompany

---
+++ 1978-EmployeesWhoseManagerLeftTheCompany/1978-EmployeesWhoseManagerLeftTheCompany.sql
@@ -0,0 +1,8 @@
+-- Last updated: 9/4/2026, 9:01:23 PM
+# Write your MySQL query statement below
+select e1.employee_id
+from Employees e1
+left join Employees e2
+on e1.manager_id = e2.employee_id
+where e1.salary < 30000 and e2.employee_id is null and e1.manager_id is not null
+order by employee_id;
