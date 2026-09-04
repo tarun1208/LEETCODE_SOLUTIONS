a46cae8f6e4a8ea2b86cbdc551c909dfddcbf7ad
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2026-09-04 21:03:19 +0530

    Create 1204-LastPersonToFitInTheBus

---
+++ 1204-LastPersonToFitInTheBus/1204-LastPersonToFitInTheBus.sql
@@ -0,0 +1,10 @@
+-- Last updated: 9/4/2026, 9:03:19 PM
+# Write your MySQL query statement below
+select person_name
+from (
+    select person_name, sum(weight) over(order by turn) as total_weight
+    from Queue
+) t
+where total_weight <= 1000
+order by total_weight desc
+limit 1
